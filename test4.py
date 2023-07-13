4b8a4410cc8401172b844ac7069cf4be03a98dce
Lily Oliphant <oliphali@oregonstate.edu>
2023-07-13 11:07:34 -0700

    Second Commit

--- test4.py
+++ test4.py
@@ -1,10 +1,7 @@
-#Lily Oliphant 
-#test interface #3
-
 import sys
 from PIL import Image
-from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QHBoxLayout, QWidget, QPushButton
-from PyQt6.QtGui import QPixmap, QPainter, QPen, QColor
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QHBoxLayout, QWidget, QPushButton, QFrame, QGridLayout
+from PyQt6.QtGui import QPixmap, QPainter, QPen
 from PyQt6.QtCore import Qt, QRect
 
 class MainWindow(QMainWindow):
@@ -12,35 +9,39 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.setWindowTitle("Apple Pruning Tutorial")
         self.setGeometry(100, 100, 800, 600)
-        
+
         # Create a main widget and set it as the central widget
         self.central_widget = QWidget(self)
         self.setCentralWidget(self.central_widget)
 
-        # Create a horizontal layout for the main widget
-        self.layout = QHBoxLayout()
-        self.central_widget.setLayout(self.layout)
-
-        # Create a QVBoxLayout for the image column
-        self.image_layout = QVBoxLayout()
-        self.layout.addLayout(self.image_layout)
+        # Create a QGridLayout for the central widget
+        self.layout = QGridLayout(self.central_widget)
 
         # Create a QLabel to display the tutorial images
-        self.image_label = QLabel()
-        self.image_layout.addWidget(self.image_label)
-
-        # Create a QVBoxLayout for the directory and buttons column
-        self.directory_layout = QVBoxLayout()
-        self.layout.addLayout(self.directory_layout)
+        self.image_label = QLabel(self.central_widget)
+        self.layout.addWidget(self.image_label, 0, 0, 2, 1)  # Row 0, Column 0, Span 2 rows and 1 column
 
         # Create a small view QLabel for the image
-        self.small_view_label = QLabel()
+        self.small_view_label = QLabel(self.central_widget)
         self.small_view_label.setFixedSize(200, 150)
-        self.directory_layout.addWidget(self.small_view_label)
+        self.layout.addWidget(self.small_view_label, 0, 1, 1, 1)  # Row 0, Column 1, Span 1 row and 1 column
+
+        # Create a QFrame for the directory and buttons column
+        self.frame = QFrame(self.central_widget)
+        self.frame.setFrameShape(QFrame.Shape.Box)
+        self.frame.setFrameShadow(QFrame.Shadow.Sunken)
+        self.layout.addWidget(self.frame, 1, 1, 1, 1)  # Row 1, Column 1, Span 1 row and 1 column
+
+        # Create a QVBoxLayout for the directory and buttons column
+        self.directory_layout = QVBoxLayout(self.frame)
 
         # Create a QLabel to display the directory
-        self.directory_label = QLabel("Current Directory:")
+        self.directory_label = QLabel("Your Task:")
         self.directory_layout.addWidget(self.directory_label)
+
+        # Create a QLabel to display the task description
+        self.task_label = QLabel()
+        self.directory_layout.addWidget(self.task_label)
 
         # Create buttons for navigation
         self.previous_button = QPushButton("Previous")
@@ -56,39 +57,34 @@ class MainWindow(QMainWindow):
         self.help_button.clicked.connect(self.show_help)
         self.directory_layout.addWidget(self.help_button)
 
-        # Define a list of tutorial images
-        try: 
-            img_1  = Image.open("branch10.jpg") 
-        except IOError:
-            pass
-         # Define a list of tutorial images
-        try: 
-            img_2  = Image.open("branch10.jpg") 
-        except IOError:
-            pass
-         # Define a list of tutorial images
-        try: 
-            img_3  = Image.open("branch10.jpg") 
-        except IOError:
-            pass
+        # Define a list of tutorial images and corresponding task descriptions
+        self.images = ["image1.jpg", "image2.jpg", "image3.jpg", "image4.jpg", "image5.jpg"]  # Replace with actual image paths
+        self.images = [r"C:\Users\lilyo\.vscode\robotics\img_1.jpg", r"C:\Users\lilyo\.vscode\robotics\img_2.jpg", r"C:\Users\lilyo\.vscode\robotics\img_3.jpg"]
+        self.tasks = [
+            "Remove vigorous wood.",
+            "Prune crossing branches.",
+            "Thin out overcrowded areas.",
+            "Remove dead or diseased wood.",
+            "Prune to encourage desired shape."
+        ]
 
-        self.images = [r"C:\Users\lilyo\.vscode\robotics\img_1.jpg"]  # Replace with actual image paths
-        
         # Initialize the current image index
         self.current_image_index = 0
-        
-        # Show the first image
 
+        # Show the first image, task, and small view image
         self.show_current_image()
 
     def show_current_image(self):
         # Get the path of the current image
         image_path = self.images[self.current_image_index]
-        
-        # Load and display the image in the main view
+
+        # Load and display the image
         pixmap = QPixmap(image_path)
         self.image_label.setPixmap(pixmap)
         self.image_label.setScaledContents(True)
+
+        # Set the current task description
+        self.task_label.setText(self.tasks[self.current_image_index])
 
         # Load and display the image in the small view
         small_pixmap = pixmap.scaled(self.small_view_label.width(), self.small_view_label.height(), Qt.AspectRatioMode.KeepAspectRatio)
@@ -98,29 +94,30 @@ class MainWindow(QMainWindow):
     def show_previous_image(self):
         # Decrease the current image index
         self.current_image_index -= 1
-        
+
         # Wrap around to the last image if necessary
         if self.current_image_index < 0:
             self.current_image_index = len(self.images) - 1
-        
-        # Show the updated image
+
+        # Show the updated image, task, and small view image
         self.show_current_image()
 
     def show_next_image(self):
         # Increase the current image index
         self.current_image_index += 1
-        
+
         # Wrap around to the first image if necessary
         if self.current_image_index >= len(self.images):
             self.current_image_index = 0
-        
-        # Show the updated image
+
+        # Show the updated image, task, and small view image
         self.show_current_image()
 
     def show_help(self):
         # Implement the functionality for the Help button
         # Display a help dialog or perform any other actions as needed
         pass
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
